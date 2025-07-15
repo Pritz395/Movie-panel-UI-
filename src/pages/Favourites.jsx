e39d7dd1d3aff6d85a87ced368be5156a70e9a6f
preetham <preethampujari395@gmail.com>
2025-07-15 17:50:20 +0530

    Initial commit – Uploading Movie Panel UI

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,29 @@
+import '../css/Favorites.css'
+import { useMovieContext } from '../contexts/MovieContext';
+import MovieCard from '../components/MovieCard';
+
+function Favourites() {
+    const { favourites } = useMovieContext();
+
+    if (favourites && favourites.length > 0) {
+        return (
+            <div className="Favourites">
+                <h2>Your Favourites</h2>
+                <div className="movies-grid">
+                    {favourites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="favourites-empty">
+            <h2>No favourites yet</h2>
+            <p>Start adding favourites and they will appear here</p>
+        </div>
+    );
+}
+
+export default Favourites;
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "9b707d23855ac72d41fff7352e13feba";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
